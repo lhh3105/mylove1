cd5447fa626b10b0527ff87281658b65ecaba7ad
lhh3105 <huuhuy11122334455@gmail.com>
2024-07-07 10:55:37 +0700

    mylove1

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Gửi đến e Thu Hà, vợ yêu của a !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Điều bất ngờ hiện ra nếu bấm vào đây`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, em không thích a :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Làm vợ a nhé Hà ơi',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100079078581493' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
